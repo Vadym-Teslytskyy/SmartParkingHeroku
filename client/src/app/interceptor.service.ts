953b3a2464042f39c3cbd97da5619d56d6f2c88e
Vadym <t.vadyk@gmail.com>
2018-04-05 17:21:14 +0300

    fix

--- client/src/app/interceptor.service.ts
+++ client/src/app/interceptor.service.ts
@@ -1,36 +1,40 @@
 import {Injectable} from '@angular/core';
 import {
+    HttpClient,
     HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,
     HttpResponse
 } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import {TokenStorage} from './auth/token/token-storage';
-import {Router} from '@angular/router';
-import {environment} from '../environments/environment';
+import {Token} from "./auth/token/token";
+import {environment} from "../environments/environment";
 
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
-
-    private domenUrl = environment.apiUrl;
 
     constructor() {
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (request.url.startsWith(this.domenUrl) && (TokenStorage.getToken())) {
-            request = request.clone({
-                headers: request.headers.append('Authorization', `Bearer ${TokenStorage.getToken()}`)
-            });
+        if (request.url.startsWith('http://localhost:8080/') && (TokenStorage.getToken())) {
+            request = InterceptorService.addAuthHeaderToRequest(request);
         }
         return next.handle(request).do((event: HttpEvent<any>) => {
             if (event instanceof HttpResponse) {
             }
         }, (error: any) => {
             if (error instanceof HttpErrorResponse) {
-
+                if(error.status === 401) {
+                    TokenStorage.saveToken(error.error);
+                }
             }
         });
     }
 
+    private static addAuthHeaderToRequest(request: HttpRequest<any>): HttpRequest<any> {
+        return request.clone({
+            headers: request.headers.append('Authorization', `Bearer ${TokenStorage.getToken()}`)
+        });
+    }
 }
